557bfac194829499a610bb6ff7cfeb9862ddd480
Om <om@Oms-MacBook-Pro.local>
2013-01-20 12:54:01 +0530

    Initial Commit

---
+++ PaintApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaintApp
+//
+//  Created by Om on 20/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PaintApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PaintApp
+//
+//  Created by Om on 20/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
